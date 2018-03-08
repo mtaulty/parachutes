9858fa9ca641cc61c6d226fa8354a468bba16f9d
Mike Taulty <mtaulty@microsoft.com>
2018-03-08 15:23:34 +0000

    Initial

---
+++ Assets/AvatarScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarScript : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!this.hasCollided)
+        {
+            this.transform.Find("Full").gameObject.SetActive(false);
+            this.transform.Find("Landed").gameObject.SetActive(true);
+            this.hasCollided = true;
+        }
+        if (hasCollided && collision.relativeVelocity == Vector3.zero)
+        {
+            this.transform.Find("Landed").gameObject.SetActive(false);
+            this.transform.Find("Stopped").gameObject.SetActive(true);
+            this.enabled = false;
+        }
+    }
+    bool hasCollided = false;
+
+}
---
+++ Assets/GlobalScript.cs
@@ -0,0 +1,24 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalScript : MonoBehaviour, IInputClickHandler {
+
+    public GameObject prefabToCreate;
+
+
+    void Start () {
+        InputManager.Instance.AddGlobalListener(this.gameObject);
+	}
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var newInstance = GameObject.Instantiate(this.prefabToCreate);
+
+        newInstance.transform.position =
+            Camera.main.transform.position +
+            Camera.main.transform.forward * 2.0f;
+
+        eventData.Use();
+    }
+}
